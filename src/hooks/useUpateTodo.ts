d31f3e0d564ba9f801a4d1982f080cf48e3828c9
bohongu <jaehong2782@naver.com>
2023-06-29 13:37:04 +0900

    Refactor: axios 인스턴스 headers (Content-Type) 적용, 인터셉터 적용

--- src/hooks/useUpateTodo.ts
+++ src/hooks/useUpateTodo.ts
@@ -1,24 +1,15 @@
 /* eslint-disable no-alert */
 import axios from "axios";
 import client from "axiosInstance/client";
+import { TodoType } from "components/Todo/types";
 import type { AxiosResponseType } from "types/types";
-import type { TodoType } from "../types";
 
 const updateTodoAPI = async (todo: TodoType): Promise<TodoType> => {
   try {
-    const response = await client.put(
-      `/todos/${todo.id}`,
-      {
-        todo: todo.todo,
-        isCompleted: todo.isCompleted,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-          "Content-Type": "application/json",
-        },
-      },
-    );
+    const response = await client.put(`/todos/${todo.id}`, {
+      todo: todo.todo,
+      isCompleted: todo.isCompleted,
+    });
     alert("수정 되었습니다!");
     return response.data;
   } catch (e) {
