e4ba654569cfb1b32dfa5b3b92d988e57b3d954d
CGH <tt878429@gmail.com>
2023-06-29 12:11:38 +0900

    Refactor: useRedirectByJwt 위치를 최상단 공통 layout으로 옮김

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,6 +4,8 @@ import { breakpoints } from "styles/breakPoints";
 import { Outlet } from "react-router-dom";
 import Header from "components/Header/Header";
 
+import useRedirectByJwt from "hooks/useRedirectByJwt";
+
 const Main = styled.main`
   position: relative;
   height: 100vh;
@@ -16,6 +18,8 @@ const Main = styled.main`
 `;
 
 function RootLayout() {
+  useRedirectByJwt();
+
   return (
     <>
       <Header />
