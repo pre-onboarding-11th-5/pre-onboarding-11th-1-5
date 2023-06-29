2c7223412798d675722294f9ee30019cc3afcdb7
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-06-29 15:30:11 +0900

    Refactor: auth 페이지 redirect

--- src/routes/rootRoute.tsx
+++ src/routes/rootRoute.tsx
@@ -1,8 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import AuthPage from "pages/authPage";
 import LandingPage from "pages/landingPage";
 import TodoPage from "pages/todoPage";
 import RootLayout from "pages/layout";
+
+function AuthRoute() {
+  const jwt = localStorage.getItem("jwt");
+  if (jwt) {
+    alert("Todo페이지로 이동합니다.");
+    return <Navigate to="/todo" replace />;
+  }
+  return <Outlet />;
+}
 
 function RootRoute() {
   return (
@@ -10,9 +25,11 @@ function RootRoute() {
       <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<LandingPage />} />
-          <Route path="signup" element={<AuthPage />} />
-          <Route path="signin" element={<AuthPage />} />
           <Route path="todo" element={<TodoPage />} />
+          <Route element={<AuthRoute />}>
+            <Route path="signup" element={<AuthPage />} />
+            <Route path="signin" element={<AuthPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
